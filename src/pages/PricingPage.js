25fb4447dacdef61200ab0429d5a2889b88bfe1a
Danialbarzegar1 <140395953+Danialbarzegar1@users.noreply.github.com>
2023-09-23 17:08:43 +0330

    Add page cpmments

--- src/pages/PricingPage.js
+++ src/pages/PricingPage.js
@@ -10,129 +10,9 @@ import im4 from "../style/im2.png";
 import im5 from "../style/im3.png";
 import im6 from "../style/ok.png";
 import im7 from "../style/no.png";
-import im9 from "../style/geometry.png";
-import im10 from "../style/logic.png";
-
-function PricingPage() {
-  const Intro = () => {
-    return (
-      <div className="Up-title">
-        <h1 className="title-1">تجربه عالی یاد گیری با ما</h1>
-        <p className="p-1">
-          دسترسی نا محدود به تمام دوره های ریاضی, علوم و علوم کامپیوتر داشته
-          باشید
-        </p>
-      </div>
-    );
-  };
-  const Box = () => {
-    return (
-      <div className="Allcard">
-        <div className="box">
-          <div className="rectangle">
-            <div className="box-up">
-              <div className="rectangle-up">
-                <div className="text-wrapper-up">ماهانه</div>
-                <div className="mony">
-                  1.300<span>/ماه</span>
-                </div>
-                <button className="btn-1">خرید اشتراک</button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="box">
-          <div className="rectangle">
-            <div className="rectangle">
-              <div className="box-up-blue">
-                <div className="rectangle-up-blue">
-                  <div className="text-wrapper-up-blue">سالانه</div>
-                  <div className="mony">
-                    1.300<span>/ماه</span>
-                  </div>
-                  <button className="btn-1">خرید اشتراک</button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="lable"></div>
-        </div>
-        <div className="box">
-          <div className="rectangle">
-            <div className="box-up">
-              <div className="rectangle-up">
-                <div className="text-wrapper-up">مادام العمر</div>
-                <div className="mony">
-                  1.300<span>/ماه</span>
-                </div>
-                <button className="btn-1">خرید اشتراک</button>
-              </div>
-            </div>
-          </div>
-          <div className="lable"></div>
-        </div>
-      </div>
-    );
-  };
-  const Titletwo = () => {
-    return (
-      <div className="label-2">
-        <p className="title-2">این سایت برای چه کسانی است؟</p>
-      </div>
-    );
-  };
-  const Boximg = () => {
-    return (
-      <div className="All-box-img">
-        <div className="box-img">
-          <div className="rectangle-img">
-            <img src={im5} className="img-1" />
-            <div>
-              <h3 className="studant">دانش اموزان</h3>
-            </div>
-            <div>
-              <p className="p-img">
-                تسلط بر مفاهیم از طریق حل مسئله در ریاضیات، علوم و علوم
-                کامپیوتر. به جای به خاطر سپردن فرمول ها، شهود بسازید.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="box-img">
-          <div className="rectangle-img">
-            <img src={im4} className="img-2" />
-            <div>
-              <h3 className="studant">حرفه ای ها</h3>
-            </div>
-            <div>
-              <p className="p-img">
-                پایگاه دانش خود را تازه نگه دارید و برای مصاحبه های کمی آماده
-                شوید. موضوعاتی از احتمال تا علوم کامپیوتر تا یادگیری ماشین را
-                کاوش کنید.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="box-img">
-          <div className="rectangle-img">
-            <img src={im3} className="img-3" />
-            <div>
-              <h3 className="studant">یادگیرندگان مادام العمر</h3>
-            </div>
-            <div>
-              <p className="p-img">
-                از بین تمرینات سرگرم کننده و هدایت شده در صدها موضوع جالب انتخاب
-                کنید. ذهن خود را فعال نگه دارید و مهارت های تفکر انتقادی را
-                بهبود بخشید.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  };
 
 import im10 from "../style/logic.png";
+
 
  function PricingPage() {
    const Intro = () => {
@@ -362,6 +242,6 @@ import im10 from "../style/logic.png";
       <Footer />
     </div>
   );
-  };
+};
 
 export default PricingPage;
