521a3150ac44b0316de38a9230d19b17aecc0ef8
Danialbarzegar1 <140395953+Danialbarzegar1@users.noreply.github.com>
2023-10-22 19:15:25 +0330

    test4

--- src/pages/PrinciplesPage.js
+++ src/pages/PrinciplesPage.js
@@ -6,20 +6,42 @@ import "./PrinciplesPage.css";
 
 function PrinciplesPage() {
     const Blackbox = () => {
-        return (
+      return (
+        <div>
           <div className="black-box">
-                <div className="black-rectangle" />
-                <div>
-                    <h2>hi</h2>
-                </div>
+            <div className="black-rectangle" />
+            <div>
+              <h2 className="h2-tit">hi</h2>
+            </div>
           </div>
-        );
+          <div className="black-box-2">
+            <div className="black-rectangle-2" />
+            <div>
+              <h2 className="h3-tit">ما عاشق❤ یادگیری هستیم</h2>
+              <p className="h4-tit">
+                ما اینجا هستیم تا کنجکاوی شما را تحریک کنیم و به شما الهام بخشیم
+                با زیبایی ریاضیات، علوم و علوم کامپیوتر.
+              </p>
+            </div>
+          </div>
+        </div>
+      );
     }
+  
+    const Titleosol = () => {
+      return (
+        <div className="white-box">
+          <p className="p-osol">اصول ما</p>
+        </div>
+      )
+  }
+    
 
   return (
     <div>
-          <Header />
-          <Blackbox />
+      <Header />
+      <Blackbox />
+      <Titleosol />
       <Footer />
     </div>
   );
--- src/pages/SingUp.js
+++ src/pages/SingUp.js
@@ -1,27 +1,28 @@
 import "./SingUp.css";
 import { Link } from "react-router-dom";
 import Button from '../component/Button';
-import luanch from '../style/Screenshot (24).png';
-
+import luanch from '../style/Screenshot (24).png'
+import React, { useState } from "react";
+import { useForm } from 'react-hook-form'
 
 function SingUp() {
-    
     return (
-        <div className="main">
-            <img src={luanch} />
-            <div className="signup">
-                <h2 className="loginn">ثبت نام</h2>
-                <input className="email" placeholder=" ایمیل"  ></input>
-                <input className="email" placeholder=" گذرواژه" ></input>
-                <div className="name-con">
-                    <input className="name1" placeholder="نام خانوادگی"></input>
-                    <input className="name" placeholder="نام"></input>
-                    
-                </div>
-                <input className="email" placeholder="سن"></input>
-                <Link to='/Educational-Site/Question1' className="button-up"><Button primary>ثبت نام</Button></Link>
-            </div>
+      <div className="main">
+        <img src={luanch} />
+        <div className="signup">
+          <h2 className="loginn">ثبت نام</h2>
+          <input  className="email" placeholder=" ایمیل" />
+          <input className="email" placeholder=" گذرواژه" />
+          <div className="name-con">
+            <input className="name1" placeholder="نام خانوادگی" />
+            <input className="name" placeholder="نام" />
+          </div>
+          <input className="email" placeholder="سن" />
+          <Link to="/Educational-Site/Question1" className="button-up">
+            <Button primary>ثبت نام</Button>
+          </Link>
         </div>
-    );
+      </div>
+  );
 }
 export default SingUp;
